7ec70af806d81b83f1eb0798a862453c519fb4b2
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-14 02:39:15 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,6 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
- *
- * Return: Always 0.
  */
 
 void print_alphabet(void)
@@ -15,5 +13,4 @@ void print_alphabet(void)
 		_putchar(x);
 	}
 	_putchar('\n');
-	return (0);
 }
