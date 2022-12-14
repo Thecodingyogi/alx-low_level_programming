328526d21b2981b3cc299def36f2c3a1ec5280ee
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-14 05:41:47 -0800

    fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,24 +12,38 @@ void times_table(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		_putchar('0');
-	}
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num1 * num2;
-
-			if (prod <= 9)
+			if ((prod / 10) == 0)
 			{
-				_putchar(' ');
+				if (num2 == 0)
+				{
+					_putchar('0');
+				}
+				if (num2 != 0)
+				{
+					_putchar(' ');
+					_putchar((prod % 10) + '0');
+				}
+				if (num2 < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+				if (num2 < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
+	}
 }
+
