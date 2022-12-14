e7195bf22976b556b47dceade8991f8ddb8ae5df
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-14 03:19:46 -0800

    Fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,12 @@ void print_alphabet_x10(void)
 	int num;
 	char x;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
+	{
 		for (x = 'a'; x <= 'z'; x++)
+		{
 			_putchar(x);
+		}
 		_putchar('\n');
+	}
 }
