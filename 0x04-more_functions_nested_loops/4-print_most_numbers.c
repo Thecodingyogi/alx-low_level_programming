9c088e628e35ae0c5e28d8fdc58e7297f2690442
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-15 12:10:47 -0800

    fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,11 @@ void print_most_numbers(void)
 	int x;
 
 	for (x = '0'; x <= '9')
-		if (!(x == '2' || x == '4'))
+	{
+		if (!(x == '2' || x== '4'))
 		{
 			_putchar(x);
 		}
+	}
 	_putchar('\n');
 }
