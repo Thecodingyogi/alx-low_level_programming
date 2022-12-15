26f25dcf43b55be99e7d13eaf39f3f72036ae6ad
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-15 12:04:00 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9')
+		if (!(x == '2' || x == '4'))
+		{
+			_putchar(x);
+		}
+	_putchar('\n');
+}
