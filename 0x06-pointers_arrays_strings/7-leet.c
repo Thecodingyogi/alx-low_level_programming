5dd295ff76d3ea8815916cfeb17b1c9ce5bab565
Thecodingyogi <cynthiatonui@gmail.com>
2022-12-22 11:59:59 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ *
+ * Return: s address
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char lower_case[] = {a, e, o, t, l};
+	char upper_case[] = {A, E, O, T, L};
+	int numbers[] = {4, 3, 0, 7, 1};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lower_case[j] || s[i] == upper_case[j])
+			{
+				s[i] == numbers[j];
+			}
+		}
+	}
+	return (s);
+}
+
