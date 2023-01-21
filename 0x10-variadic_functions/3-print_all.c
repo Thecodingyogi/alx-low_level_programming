69432887aebbbdb86205d36e91f14d24689b087d
Thecodingyogi <cynthiatonui@gmail.com>
2023-01-21 14:27:42 -0800

    fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,7 +40,7 @@ void print_all(const char * const format, ...)
 			result = 1;
 			break;
 		}
-		if (format[i + 1] != '0' && result == 0)
+		if (format[i + 1] != '\0' && result == 0)
 			printf(", ");
 		i++;
 	}
