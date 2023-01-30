69940e407000044fcbb42844e5b6fe7b235eb018
Thecodingyogi <cynthiatonui@gmail.com>
2023-01-30 10:36:20 -0800

    updated

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,6 +28,8 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
 
+	if (str == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
@@ -39,13 +41,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new->len = _strlen(new->str);
 	new->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
 	temp = *head;
-
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new;
