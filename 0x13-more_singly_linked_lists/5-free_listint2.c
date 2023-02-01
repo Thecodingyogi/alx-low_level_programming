ceda10c741caa24a2f8ca68a44499e24b54ec5ed
Thecodingyogi <cynthiatonui@gmail.com>
2023-02-01 09:30:57 -0800

    Updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,13 +11,12 @@ void free_listint2(listint_t **head)
 
 	if (head == NULL)
 		return;
-
-		while (*head != NULL)
-		{
-			temp = (*head)->next;
-			free(*head);
-			*head = temp;
-		}
-		*head = NULL;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
 }
 
