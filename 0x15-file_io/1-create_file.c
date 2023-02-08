f0b29321400fc33fa8b59a03edea1668c405d9b2
Thecodingyogi <cynthiatonui@gmail.com>
2023-02-08 09:50:49 -0800

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,15 +10,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
+		while (text_content[len] != NULL)
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
