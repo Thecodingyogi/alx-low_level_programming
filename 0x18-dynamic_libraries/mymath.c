286e793041f8a7a12b6bbf0c4ea9556c1d672e7d
Thecodingyogi <cynthiatonui@gmail.com>
2023-03-20 06:21:51 -0700

    Advanced task

---
+++ 0x18-dynamic_libraries/mymath.c
@@ -0,0 +1,64 @@
+/**
+ * add - adds two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: quotient of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - prints remainder of two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: remainder of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
